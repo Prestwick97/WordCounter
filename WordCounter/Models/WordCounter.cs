79de92bd6cfac2eb44b33bf7c7e9dba70ca5a0dc
Student <example@example.com>
2020-03-06 12:02:52 -0800

    add alphabet list

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,12 +5,15 @@ using System.Linq;
 
 namespace WordCounter.Models
 {
+
   public class WordCount
   {
+    List<string> alphabetList = new List<string> {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
     public bool LetterCheck(string input)
     {
-      if(!input.Contains("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"))
+      
+      if(!input.Contains(alphabetList)
       {
         Console.WriteLine("Please make sure your input consists of only letters.");
       }
