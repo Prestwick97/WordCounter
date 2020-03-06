158fa048f769236ac1f8179cfa7e4e5165aa5608
Prestwick97 <prestwick97@gmail.com>
2020-03-06 09:55:35 -0800

    change letter check method into bool

---
+++ WordCounter.Tests/Models.Tests/WordCounter.Tests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Test
+{
+
+  [TestClass]
+  public class WordCountTests
+  {
+
+    [TestMethod]
+    public void LetterCheck_RecieveOnlyLetters_
+  }
+}
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,11 +5,15 @@ namespace WordCounter.Models
   public class WordCount
   {
 
-    public void LetterCheck(string input)
+    public bool LetterCheck(string input)
     {
-      if(!input.contain("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"))
+      if(!input.Contains("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"))
       {
         Console.WriteLine("Please make sure your input consists of only letters.");
+      }
+      else
+      {
+        return true;
       }
     }
   }
